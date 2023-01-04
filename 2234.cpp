9c5a2df7e25490251705df3aebd6a9ea9be078a1
Mayara <mayaraassisn@gmail.com>
2023-01-03 22:38:33 -0300

    Desafio 2234 Beecrowd

---
+++ 2234.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1,n2;
+    double saida;
+    cin>>n1>>n2;
+    saida = double(n1)/n2;
+
+    printf("%.2lf\n", saida);
+
+    return 0;
+}
