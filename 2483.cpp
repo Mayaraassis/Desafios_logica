a361f483aa3d094a5634eae18c23c5af887cd59a
Mayara <mayaraassisn@gmail.com>
2023-01-04 22:51:53 -0300

    Beecrowd 2483

---
+++ 2483.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i;
+    cin>>i;
+
+
+    string str;
+    for(int j=0; j<i; j++)
+    {
+        str+= 'a';
+    }
+
+    cout<<"Feliz nat"<<str<<"l!"<<endl;
+
+}
