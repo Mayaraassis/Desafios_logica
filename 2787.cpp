303b87221f52a0366827a1a9bd647722f5d004cc
Mayara <mayaraassisn@gmail.com>
2023-01-16 21:22:47 -0300

    2787 Beecrowd

---
+++ 2787.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int L,C;
+
+    cin>>L>>C;
+
+    if((L+C)%2 == 0)
+    {
+        cout<<"1"<<endl;
+    }
+    else
+    {
+        cout<<"0"<<endl;
+    }
+
+}
