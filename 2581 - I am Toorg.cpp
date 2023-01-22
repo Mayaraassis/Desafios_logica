77c8240587123d09057dc4826fe244eb9f177170
Mayara <mayaraassisn@gmail.com>
2023-01-21 22:17:16 -0300

    2581 -  I am Toorg - Beecrowd

---
+++ 2581 - I am Toorg.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin>>N;
+    string str;
+    for(int i=0; i<N; i++)
+    {
+        getline(cin >> ws, str);
+        cout<<"I am Toorg!"<<endl;
+
+    }
+
+}
