1b5adf978b73b3f70bafd5c26b86bb371a13a94f
Mayara <mayaraassisn@gmail.com>
2023-01-24 23:19:53 -0300

    2752 - Saída 6 - Beecrowd

---
+++ 2752 - Saida 6.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+cout<<"<AMO FAZER EXERCICIO NO URI>"<<endl;
+cout<<"<    AMO FAZER EXERCICIO NO URI>"<<endl;
+cout<<"<AMO FAZER EXERCICIO >"<<endl;
+cout<<"<AMO FAZER EXERCICIO NO URI>"<<endl;
+cout<<"<AMO FAZER EXERCICIO NO URI    >"<<endl;
+cout<<"<AMO FAZER EXERCICIO NO URI>"<<endl;
+cout<<"<          AMO FAZER EXERCICIO >"<<endl;
+cout<<"<AMO FAZER EXERCICIO           >"<<endl;
+
+}
+
+
+
+
+
+
+
+
+
