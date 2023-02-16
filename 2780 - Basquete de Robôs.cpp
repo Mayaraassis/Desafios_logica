31ecb2706778a6cf3193aeaab604dde633444ac5
Mayara <mayaraassisn@gmail.com>
2023-02-15 21:19:55 -0300

    2780 - Basquete de Robôs (beecrowd)

---
+++ 2780 - Basquete de Robôs.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int D;
+    cin>>D;
+
+    if(D<=800)
+    {
+        cout<<"1"<<endl;
+    }
+    else if(D>800 && D<=1400)
+    {
+
+        cout<<"2"<<endl;
+    }
+    else
+    {
+        cout<<"3"<<endl;
+
+    }
+
+
+}
