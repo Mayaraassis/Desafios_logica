3817e5623cfd6560fb2343e6529cea46ed065fd6
Mayara <mayaraassisn@gmail.com>
2023-02-18 12:56:13 -0300

    Problem 2: Even Fibonacci Numbers (FreeCodeCamp)

---
+++ Problem 2: Even Fibonacci Numbers.js
@@ -0,0 +1,32 @@
+function fiboEvenSum(n) {
+    const acc = []
+    let i = 1;
+    let j = 1;
+    let aux = 0;
+    let divTwo = 0
+    while(aux <= n){
+        if(i <= 2) {
+            aux = i
+            acc.push(aux)
+            i++
+        } else {
+            aux = acc[j] + acc[j - 1]
+            if(aux<=n){
+                acc.push(aux)
+                j++
+            }
+            else{
+                break;
+            }
+        }
+    }
+    for(let i = 0; i < acc.length; i++){
+        if(acc[i] % 2 === 0){
+            divTwo += acc[i]
+        }
+    }
+    console.log(acc)
+    console.log(divTwo)
+}
+
+fiboEvenSum(1000)
