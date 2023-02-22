f2f073c264fb1919742dd45d1a31c62f98e97c42
Mayara <mayaraassisn@gmail.com>
2023-02-21 23:19:58 -0300

    2936 - Quanta Mandioca? (Beecrowd)

---
+++ 2936 - Quanta Mandioca?.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c,d,e,soma;
+
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+
+    soma = a*300+b*1500+c*600+d*1000+e*150 + 225;
+
+    printf("%d\n",soma);
+
+    return 0;
+}
