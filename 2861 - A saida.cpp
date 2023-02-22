44d410cd639bba4df4ad35cfc261f33c84090a2a
Mayara <mayaraassisn@gmail.com>
2023-02-22 00:31:23 -0300

    2861 - A saida (beecrowd)

---
+++ 2861 - A saida.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int C;
+    string str;
+    cin>>C;
+    for(int i=0; i<C; i++)
+    {
+        cin>>str;
+        cout<<"gzuz"<<endl;
+    }
+}
