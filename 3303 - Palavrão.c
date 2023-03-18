22b793f2bd5b037a65cf083ba00651e6a97dafe7
Mayara <mayaraassisn@gmail.com>
2023-03-18 17:41:23 -0300

    3303 - Palavrão (Beecrowd)

---
+++ 3303 - Palavrão.c
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin>>str;
+    if(str.length()>=10){
+        cout<<"palavrao"<<endl;
+    }
+    else {
+        cout<<"palavrinha"<<endl;
+    }
+
+    return 0;
+}
