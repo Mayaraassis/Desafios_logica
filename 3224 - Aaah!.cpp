524fa5a1fdd2ea9002769ef4b65a0e1e5ed21ff9
Mayara <mayaraassisn@gmail.com>
2023-04-01 21:09:06 -0300

    3224 - Aaah! (Beecrowd)

---
+++ 3224 - Aaah!.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string ah, ah2;
+    cin>>ah>>ah2;
+    if(ah.length() < ah2.length()){
+        cout<<"no"<<endl;
+    }
+    else{
+        cout<<"go"<<endl;
+    }
+
+    return 0;
+}
