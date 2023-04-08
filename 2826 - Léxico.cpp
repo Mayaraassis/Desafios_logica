17f802c6a98979b48cfe4ee3ccf56ee0d4c56005
Mayara <mayaraassisn@gmail.com>
2023-04-07 23:43:17 -0300

    2826 - Léxico (Beecrowd)

---
+++ 2826 - Léxico.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string A, B;
+    cin>>A>>B;
+    if(A<B){
+        cout<<A<<endl;
+        cout<<B<<endl;
+    }
+    else {
+        cout<<B<<endl;
+        cout<<A<<endl;
+    }
+
+    return 0;
+}
