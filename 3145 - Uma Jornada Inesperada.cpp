8e6b58e37498902bcd560ab522ed5ad87eb75733
Mayara <mayaraassisn@gmail.com>
2023-04-11 23:24:46 -0300

    3145 - Uma Jornada Inesperada (Beecrowd)

---
+++ 3145 - Uma Jornada Inesperada.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, X;
+    double saida=0;
+    cin>>N>>X;
+    saida = (double)X/(N+2);
+    printf("%.2lf\n",saida);
+
+    return 0;
+}
