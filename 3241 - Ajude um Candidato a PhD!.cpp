a98f8d0b25183cd46acfa924d016b0478f289f8b
Mayara <mayaraassisn@gmail.com>
2023-04-11 23:29:33 -0300

    Update 3241 - Ajude um Candidato a PhD!.cpp

--- 3241 - Ajude um Candidato a PhD!.cpp
+++ 3241 - Ajude um Candidato a PhD!.cpp
@@ -1,23 +1,27 @@
 
-#include <iostream>
-#include <string> 
-using namespace std;
+#include <stdio.h>
 
-int main()
-{
-    int N, soma=0;
-    cin>>N;
-    string a,b,op;
-    for(int i=0;i<N;i++){
-        cin>>a>>op>>b;
-        if(op=="="){
-            cout<<"skipped"<<endl;
+int main() {
+    int N,i,j;
+    char operador,lixo[10];
+    int a,b;
+    scanf("%d",&N);
+
+    for(i=0; i<N; i++){
+        scanf(" %d",&a);
+        scanf(" %c",&operador);
+        scanf(" %d",&b);
+
+        if (operador != '+'){
+            scanf(" %s",lixo);
+            printf("skipped\n");
+        }else{
+            a = a + b;
+            printf("%d\n",a);
         }
-        else{
-            soma = stoi(a) + stoi(b);
-            cout<<soma<<endl;
-        }
+        
     }
+
 
     return 0;
 }
