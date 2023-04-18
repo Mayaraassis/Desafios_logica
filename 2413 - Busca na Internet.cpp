fd41d22f28988619447db77b4a550f67a63e0dd2
Mayara <mayaraassisn@gmail.com>
2023-04-17 22:38:35 -0300

    2413 - Busca na Internet (Beecrowd)

---
+++ 2413 - Busca na Internet.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, saida=0;
+    cin>>N;
+    saida = N*2;
+    saida = saida * 2;
+    cout<<saida<<endl;
+
+    return 0;
+}
