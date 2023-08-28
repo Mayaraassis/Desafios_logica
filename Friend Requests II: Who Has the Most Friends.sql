32c197b31d37f632ee06d7dd2f02d1fc73bf3512
Mayara <mayaraassisn@gmail.com>
2023-08-27 23:01:54 -0300

    Create Friend Requests II: Who Has the Most Friends.sql

---
+++ Friend Requests II: Who Has the Most Friends.sql
@@ -0,0 +1,9 @@
+select id , count(id) as num
+from (
+
+    select requester_id as id from requestaccepted
+    union all
+    select accepter_id from requestaccepted
+)s
+group by id
+order by count(id) desc limit 1 ;
