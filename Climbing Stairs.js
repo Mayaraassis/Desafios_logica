a3cd4974c7375a5435d5925b86670133f2a40bb0
Mayara <mayaraassisn@gmail.com>
2023-09-25 23:49:57 -0300

    Create Climbing Stairs.js

---
+++ Climbing Stairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+ let mem=[]
+var climbStairs = function(n) {
+    if(n<=2) return n;
+    if(mem[n]!=undefined) return mem[n]
+    mem[n]=climbStairs(n-1)+climbStairs(n-2)
+    return mem[n]
+};
