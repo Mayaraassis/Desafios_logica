bd4bae3885ce784247b8c20d3aff02dd5de3731d
Mayara <mayaraassisn@gmail.com>
2023-12-22 23:12:33 -0300

    Create Add Two Promises.js

---
+++ Add Two Promises.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    val = await promise1
+    val2 = await promise2
+    return val + val2
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
